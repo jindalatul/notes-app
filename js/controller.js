9abbe24c13613015c03377423dd42427f94b1169
Atul Jindal <er.atul.jindal@gmail.com>
2017-05-08 12:39:06 -0600

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,109 @@
+/**
 
+This is main Controller of the application, interacting with view and data.
+
+**/
+
+window.addEventListener("load",init);
+window.noteCounter=0;
+
+function init(){
+	
+	document.getElementById("add-notes-option").addEventListener("click",showAddNotesDialog);
+	
+	document.getElementById("cancel-button").addEventListener("click",function(){
+			document.getElementById("form-box").style.transform="translateX(-100%)";
+	});
+
+	document.getElementById("delete-all-option").addEventListener("click",deleteAllNotes);
+	document.getElementById("delete-button").addEventListener("click",deleteNote)
+
+		
+    document.getElementById("save-button").addEventListener("click",saveAction);
+	
+    document.getElementById("noteName").addEventListener("keyup",checkEmpty);   
+   
+}
+
+function showAddNotesDialog()
+{
+		document.getElementById("form-box").children[0].children[0].innerHTML = "Add New Note";
+		document.getElementById("noteName").value = "";
+		document.getElementById("color").value= "default";
+		document.getElementById("nid").value=0;
+		document.getElementById("delete-button").style.display="none";
+		document.getElementById("form-box").style.transform="translateX(100%)";
+}
+
+
+function ShowEditNotesDialog()
+{
+	document.getElementById("form-box").children[0].children[0].innerHTML = "Updates Note";
+	document.getElementById("noteName").value = this.innerHTML;
+	document.getElementById("color").value= this.style.color;
+	document.getElementById("nid").value=this.getAttribute("nid");
+	document.getElementById("delete-button").style.display="inline-block";
+	document.getElementById("form-box").style.transform="translateX(100%)";
+}
+
+function checkEmpty(){
+    var noteName = document.getElementById(this.id).value;
+    if(noteName){
+        document.getElementById("noteNameError").innerHTML="";
+    }
+}
+
+function deleteNote()
+{
+	var nid = document.getElementById("nid").value;
+	noteOperation.deleteNote(nid);
+	printNotes();	
+	document.getElementById("form-box").style.transform="translateX(-100%)";
+}
+
+function deleteAllNotes()
+{
+	noteOperation.emptyNotes();
+	//// Delete all notes-option
+	document.getElementById("notes-section").children[0].innerHTML="";
+}
+
+function saveAction()
+{
+		var noteName = document.getElementById("noteName").value;
+		var color = document.getElementById("color").value;
+		var nid = document.getElementById("nid").value;
+        	
+		if(!noteName){
+            document.getElementById("noteNameError").innerHTML="noteName Can't Be Empty";
+            return ;
+        }
+		
+        if(nid>0)
+				noteOperation.updateNotes(nid,noteName,color);
+        else
+				noteOperation.addNotes(noteName,color);
+		
+		printNotes();
+
+		document.getElementById("form-box").style.transform="translateX(-100%)";
+}
+
+function printNotes()
+{
+	  var ul = document.getElementById("notes-section").children[0];
+	  var notes = noteOperation.noteList;
+	  
+	  ul.innerHTML="";
+	  
+	  notes.forEach(function(currentNote){
+	   var li = document.createElement("li");
+
+		li.setAttribute("nid",currentNote.nid);
+		li.style.color = currentNote.color;
+		li.addEventListener("click",ShowEditNotesDialog);
+	    li.innerHTML = currentNote.name;
+        ul.appendChild(li);
+	  });
+	  	
+}
--- js/note.js
+++ js/note.js
@@ -1,1 +1,50 @@
+/* 
+	Data and its operations
+*/
+function Note(name, color){
+    this.nid = ++window.noteCounter;
+	this.name = name;
+    this.color = color;
+}
 
+var noteOperation = {
+    noteList:[],
+    addNotes:function(name,color){
+        var note = new Note(name,color);
+        this.noteList.push(note);
+    },
+	updateNotes:function(nid,name,color){
+			
+				//alert("length"+this.noteList.length);
+			
+				this.noteList.forEach(function(currentNote){
+			
+				if(currentNote.nid == nid)
+				{
+					currentNote.name=name;
+					currentNote.color=color;
+					return true;
+				}
+			});
+	},
+	deleteNote:function(nid){
+		//console.log(nid);
+		
+		//filter to delete notes
+		this.noteList = this.noteList.filter(function(note){
+				return (note.nid!=nid);
+		});
+		
+		//alert(result.length);
+	}
+	,
+	emptyNotes:function()
+	{
+		if(this.noteList.length<=0)
+		{
+			alert("No Notes are added");
+			return false;
+		}
+		this.noteList.length=0;
+	}
+}
